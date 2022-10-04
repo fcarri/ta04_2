9af9b46b425ce5f7c9c5b9640238d9f854693982
fcarri <fcs0000@gmail.com>
2022-10-04 16:44:51 +0200

    Commit 5

---
+++ ta04_2.java
@@ -0,0 +1,21 @@
+package project5;
+
+public class ta04_2 {
+	public static void main(String args[]){
+		
+		int N = 1;
+		double A = 2;
+		char C = 'C';
+		double sum = N + A;
+		double resta = A - N; 
+		
+		int CC = (int)(C);
+		
+		System.out.println("N = "+N);
+		System.out.println("A = "+A);
+		System.out.println("C = "+C);
+		System.out.println("N + A = "+sum);
+		System.out.println("A - N = "+resta);
+		System.out.println("C = "+CC);
+	}
+}
